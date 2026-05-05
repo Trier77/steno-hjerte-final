b5d9e4fde7bfdf770fb38741a1d336e93ea5e4db
Trier77 <jacob.aa.trier@gmail.com>
2026-05-05 10:26:52 +0200

    leger med tailwind

--- src/pages/StartSide.jsx
+++ src/pages/StartSide.jsx
@@ -1,8 +1,12 @@
-import nyfront from '../assets/nyfront.png'
+import krop from '../assets/kropudenbg.svg'
 
 function StartSide(){
     return(
-        <h1>do you see dis</h1>
+        <>
+        <h1 className="titel z-0 text-amber-300">do you see dis</h1>
+        <img src={krop} className="frontbg" width="1920px" height="1080px z-1" alt="" />
+        </>
+        
         
     )
 }
