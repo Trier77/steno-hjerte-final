e5ed20366126e8902e4c0130d884e2d97cc99fe2
Trier77 <jacob.aa.trier@gmail.com>
2026-05-09 12:31:39 +0200

    ændrer layout p.g.a. layout fejl

--- src/pages/Graviditet.jsx
+++ src/pages/Graviditet.jsx
@@ -1,5 +1,6 @@
 import BackButton from "../components/BackButton";
 import FlagButton from "../components/FlagButton";
+import Speedometer from "../components/Speedometer";
 
 function Graviditet() {
   return (
@@ -7,6 +8,8 @@ function Graviditet() {
       <h1>Du er havnet på Graviditets-siden</h1>
       <FlagButton />
       <BackButton />
+      
+    <Speedometer />
     </>
   );
 }
