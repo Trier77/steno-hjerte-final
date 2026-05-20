8024bce4b987f4c4e951679c8445e7fa75ee3ea5
Trier77 <jacob.aa.trier@gmail.com>
2026-05-20 12:28:48 +0200

    tilføjet build:pi og build:gh i package.json og justeret vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) =>{
-  const isGithubPages = process.env.BUILD_TARGET === 'raspberry';
-
-  return{
-    base: command === "serve" ? "/" : "/steno-hjerte2/",
-  plugins: [react(), legacy({
-      targets: ['chrome >= 69'],   // BrightSign HD5 OS9 kører Chromium ~69-94
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ['chrome >= 69'],
       renderLegacyChunks: true,
-    }), tailwindcss()],
-  }
-  
+    }),
+    tailwindcss(),
+  ],
 })
